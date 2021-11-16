89fa30b4974dc432bc6881cb9e9e3e7ac81a2041
Cmoo128 <78281065+Cmoo128@users.noreply.github.com>
2021-11-16 12:52:20 -0600

    Update main.dart
    Added search feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'user.dart';
 import 'collectible.dart';
@@ -8,7 +9,7 @@ void main() {
   // var allCollectibles = <Collectible>[];
   void addCollectible(User poster, String name, String description, {double price = -1.0, String image = ""})
   {
-    print("Hi");
+    print("Hey");
     Collectible newPost = Collectible(name, description, poster.getUsername, price: price, imagePath: image);
     allCollectibles.add(newPost);
     poster.addItem(newPost);
@@ -16,16 +17,16 @@ void main() {
 
   User logan = User("Ignie", "Logan12345");
   addCollectible(logan, 'Spiderman Torment Part 1',
-    'This is the first issue of the Spiderman Torment story line.',
-    image: 'assets/SM_Comic.JPEG');
+      'This is the first issue of the Spiderman Torment story line.',
+      image: 'assets/SM_Comic.JPEG');
 
   addCollectible(logan, 'Childish Gambino Because of The Internet Vinyl',
-    'This is a vinyl of Donald Glover\'s second studio album.',
-    price: 49.95, image: 'assets/Childish_Vinyl.JPEG');
+      'This is a vinyl of Donald Glover\'s second studio album.',
+      price: 49.95, image: 'assets/Childish_Vinyl.JPEG');
 
   addCollectible(logan, 'Luffy Gear Four Funko POP',
-    'Limited Chalice Collectibles exclusive funko pop. It\'s been taken out of the box but kept in great condition and the box is undamaged.',
-    price: 45, image:'assets/Luffy_Pop.JPEG');
+      'Limited Chalice Collectibles exclusive funko pop. It\'s been taken out of the box but kept in great condition and the box is undamaged.',
+      price: 45, image:'assets/Luffy_Pop.JPEG');
 
   runApp(
       MaterialApp(
@@ -50,159 +51,159 @@ class _CollectDispState extends State<CollectDisp> {
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-          appBar: AppBar(
-            title: Center(
-              child: Text(
-                'CollectR',
-                style: TextStyle(
-                  fontSize:  20.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.amber[300],
-                  fontFamily: 'Cairo',
-                ),
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'CollectR',
+              style: TextStyle(
+                fontSize:  20.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.amber[300],
+                fontFamily: 'Cairo',
               ),
             ),
-            backgroundColor: Colors.grey[900],
           ),
-          body: Container(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                top:    15.0,
-                bottom: 15.0,
-              ),
-              child: Center(
-                  child: Container(
-                    child: ListView(
-                      children: <Widget>[
-                        Column(
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(bottom: 10.0),
-                              child: Text(
-                                allCollectibles.elementAt(1).getName,
-                                style: TextStyle(
-                                  fontSize:  20.0,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.amber[300],
-                                  fontFamily: 'Cairo',
-                                ),
+          backgroundColor: Colors.grey[900],
+        ),
+        body: Container(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top:    15.0,
+              bottom: 15.0,
+            ),
+            child: Center(
+                child: Container(
+                  child: ListView(
+                    children: <Widget>[
+                      Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 10.0),
+                            child: Text(
+                              allCollectibles.elementAt(1).getName,
+                              style: TextStyle(
+                                fontSize:  20.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.amber[300],
+                                fontFamily: 'Cairo',
                               ),
                             ),
-                            Container(
-                              child: Row(
-                                children: [
-                                  Spacer(),
-                                  Spacer(),
-                                  Text(
-                                    allCollectibles.elementAt(1).getOwner,
-                                    style: TextStyle(
-                                      fontSize:  13.0,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.amber[300],
-                                      fontFamily: 'Cairo',
-                                    ),
-                                  ),
-                                  Spacer(),
-                                  Text((() {
-                                    if(allCollectibles.elementAt(2).getPrice == -1){
-                                      return '';}
-
-                                    return '\$'+allCollectibles.elementAt(2).getPrice.toString();}
-                                  )(),
-                                    style: TextStyle(
-                                      fontSize:  13.0,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.amber[300],
-                                      fontFamily: 'Cairo',
-                                    ),
-                                  ),
-                                  Spacer(),
-                                  Spacer(),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                        Spacer(),
-                        Container(
-                          child: Image.asset(allCollectibles.elementAt(1).getImagePath),
-                          height: 350,
-                          width: 350,
-                        ),
-                        Spacer(),
-                        Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Container(
-                            child: Padding(
-                              padding: EdgeInsets.all(10.0),
-                              child: Text(
-                                allCollectibles.elementAt(1).getDescription,
-                                style: TextStyle(
-                                  fontSize:  15.0,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.amber[300],
-                                  fontFamily: 'Cairo',
-                                ),
-                              ),
-                            ),
-                            color: Colors.grey[900],
                           ),
-                        ),
-
                           Container(
-
-                            child: TextButton(
-                              onPressed: (){},
-                              child: Text(
-                                'Amogus',
-                                style: TextStyle(
-                                  fontSize:  15.0,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey[900],
-                                  fontFamily: 'Cairo',
+                            child: Row(
+                              children: [
+                                Spacer(),
+                                Spacer(),
+                                Text(
+                                  allCollectibles.elementAt(1).getOwner,
+                                  style: TextStyle(
+                                    fontSize:  13.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.amber[300],
+                                    fontFamily: 'Cairo',
+                                  ),
                                 ),
+                                Spacer(),
+                                Text((() {
+                                  if(allCollectibles.elementAt(2).getPrice == -1){
+                                    return '';}
+
+                                  return '\$'+allCollectibles.elementAt(2).getPrice.toString();}
+                                )(),
+                                  style: TextStyle(
+                                    fontSize:  13.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.amber[300],
+                                    fontFamily: 'Cairo',
+                                  ),
+                                ),
+                                Spacer(),
+                                Spacer(),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Spacer(),
+                      Container(
+                        child: Image.asset(allCollectibles.elementAt(1).getImagePath),
+                        height: 350,
+                        width: 350,
+                      ),
+                      Spacer(),
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Container(
+                          child: Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text(
+                              allCollectibles.elementAt(1).getDescription,
+                              style: TextStyle(
+                                fontSize:  15.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.amber[300],
+                                fontFamily: 'Cairo',
                               ),
                             ),
-                            color: Colors.amber[300],
-
                           ),
+                          color: Colors.grey[900],
+                        ),
+                      ),
+
+                      Container(
+
+                        child: TextButton(
+                          onPressed: (){},
+                          child: Text(
+                            'Amogus',
+                            style: TextStyle(
+                              fontSize:  15.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey[900],
+                              fontFamily: 'Cairo',
+                            ),
+                          ),
+                        ),
+                        color: Colors.amber[300],
+
+                      ),
 
 
 
 
 
 
-                      ],
-                    ),
-                    color: Colors.grey[850],
-                  )
+                    ],
+                  ),
+                  color: Colors.grey[850],
+                )
+            ),
+          ),
+          color: Colors.black,
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            children: [
+              IconButton(
+                icon: Icon(Icons.home), onPressed: () {},
+                color: Colors.amber[400],
               ),
-            ),
-            color: Colors.black,
-          ),
-          bottomNavigationBar: BottomAppBar(
-            child: Row(
-              children: [
-                IconButton(
-                  icon: Icon(Icons.home), onPressed: () {},
-                  color: Colors.amber[400],
-                ),
 
-                Spacer(),
-                IconButton(
-                  icon: Icon(Icons.search), onPressed: () {},
-                  color: Colors.amber[400],
-                ),
-                Spacer(),
-                IconButton(
-                  icon: Icon(Icons.person_rounded), onPressed: () {},
-                  color: Colors.amber[400],
-                ),
+              Spacer(),
+              IconButton(
+                icon: Icon(Icons.search), onPressed: () {},
+                color: Colors.amber[400],
+              ),
+              Spacer(),
+              IconButton(
+                icon: Icon(Icons.person_rounded), onPressed: () {},
+                color: Colors.amber[400],
+              ),
 
-              ],
-            ),
-            color: Colors.grey[900],
+            ],
           ),
+          color: Colors.grey[900],
+        ),
 
       ),
       color: Colors.grey[850],
@@ -264,7 +265,7 @@ class Home extends StatelessWidget {
                               MaterialPageRoute(
                                   builder: (context) => CollectDisp()
 
-                            ),
+                              ),
                             );
                           },
                           child: Column(
@@ -324,11 +325,16 @@ class Home extends StatelessWidget {
               color: Colors.amber[400],
             ),
 
+
             Spacer(),
             IconButton(
-              icon: Icon(Icons.search), onPressed: () {},
+              icon: Icon(Icons.search), onPressed: () {
+                showSearch(context: context, delegate: DataSearch());
+            },
               color: Colors.amber[400],
             ),
+
+
             Spacer(),
             IconButton(
               icon: Icon(Icons.person_rounded), onPressed: () {},
@@ -347,5 +353,86 @@ class Home extends StatelessWidget {
 
 
     );
+  }
+}
+
+
+
+class DataSearch extends SearchDelegate<String?> {
+
+
+  final recentSearches = [
+    "Lego AT-TE",
+    "Saul Goodman Funko Pop",
+    "Vintage CD",
+    "Spiderman Torment Part 1",
+  ];
+
+  final testCollectibles = [
+    "Lego AT-TE",
+    "Saul Goodman Funko Pop",
+    "Vintage CD",
+    "Fortnite Chungus",
+    "Spiderman Torment Part 1",
+    "Luffy Gear Four Funko POP",
+    "Childish Gambino Because of The Internet Vinyl",
+    "Five Nights at Freddys Plushy",
+    "Scooby Doo Bobblehead",
+    "Babe Ruth Baseball Card",
+    "Jack Winstead Stop Sign",
+    "Funko Pop",
+    "Vinyl",
+  ];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+        onPressed: () {
+      query = "";
+    })
+    ];
+  }
+
+  String? fix = null;
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow,
+          progress: transitionAnimation,
+    ),
+    onPressed: () {
+      close(context, null);
+    });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+   final List suggestionList = query.isEmpty ? recentSearches : testCollectibles.where((p) => p.startsWith(query)).toList();
+   
+   return ListView.builder(
+       itemBuilder: (context, index) => ListTile(
+     leading: const Icon(Icons.location_city),
+         title: RichText(text: TextSpan(
+           text: suggestionList[index].substring(0,query.length),
+           style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),
+             children: [
+               TextSpan(
+             text: suggestionList[index].substring(query.length),
+           style: TextStyle(color: Colors.grey))
+         ],
+       ),
+      ),
+    ),
+    itemCount: suggestionList.length,
+   );
   }
 }
